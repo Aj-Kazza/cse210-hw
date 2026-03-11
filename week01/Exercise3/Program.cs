864c36b483057eeb1a5e534eac809599babf7d6d
Aj-Kazza <akatia@byupathway.edu>
2026-03-12 01:27:38 +1200

    finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string play = "yes";
+
+        while (play != "no")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+           string correct = "no";
+            int guess = -1;
+            int guesses = 0;
+
+
+            while (correct == "no")
+            {
+                Console.Write("What is the magic number? ");
+                guess = int.Parse(Console.ReadLine());
+                guesses += 1;
+
+                if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! The magic number is {number}");
+                    Console.WriteLine($"You've guessed {guesses} amount of time(s)");
+                    correct = "yes";
+                    
+                    Console.Write("Do you want to play again?(yes/no) ");
+                    string reply = Console.ReadLine();
+                    if (reply == "no")
+                    {
+                        play = "no";
+                    }
+
+                }
+
+                
+
+            }
+
+        }
     }
 }
