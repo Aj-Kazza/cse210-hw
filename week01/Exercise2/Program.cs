d2925083386797b0cc932ed7ae1351cc02b0be03
Aj-Kazza <akatia@byupathway.edu>
2026-03-12 00:50:05 +1200

    finished exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string grade = "";
+        string signifier = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+            if (percent < 93)
+            {
+                signifier = "-";
+            }
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+            if (percent >= 87)
+            {
+                signifier = "+";
+            }
+            else if (percent < 83)
+            {
+                signifier = "-";
+            }
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+            if (percent >= 77)
+            {
+                signifier = "+";
+            }
+            else if (percent < 73)
+            {
+                signifier = "-";
+            }
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+            if (percent >= 67)
+            {
+                signifier = "+";
+            }
+            else if (percent < 63)
+            {
+                signifier = "-";
+            }
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}{signifier}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed!!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed!!");
+        }
     }
 }
