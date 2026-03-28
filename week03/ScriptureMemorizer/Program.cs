bcead70d99f1da2410109d05e82881d12b1bb395
Aj-Kazza <akatia@byupathway.edu>
2026-03-29 08:05:10 +1200

    week03 scripture memorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        bool isCompletelyHidden = false;
+
+        Reference reference = new Reference("John", 14, 15);
+        Scripture scripture = new Scripture(reference, "If ye love me, keep my commandments");
+
+        Console.WriteLine(reference.GetDisplayText());
+        Console.WriteLine(scripture.GetDisplayText());
+
+        while (isCompletelyHidden != true)
+        {
+            
+            
+        }
+
+
+        Random random = new Random();
+        int num = random.Next(0,6);
+        Console.WriteLine(num);
+        scripture.HideRandomWords(num);
+        Console.WriteLine(scripture.GetDisplayText());
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,44 @@
+using System;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        List<string> texts = text.Split(" ").ToList();
+        List<Word> words = new List<Word>();
+        foreach (string i in texts)
+        {
+            Word word = new Word(i);
+            words.Add(word);
+        }
+        _words = words;
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        _words[numberToHide].Hide();
+    }
+
+
+    public string GetDisplayText()
+    {
+        List<string> texts = new List<string>();
+        foreach (Word word in _words)
+        {
+            texts.Add(word.GetDisplayText());
+        }
+        
+        string text = String.Join(" ", texts);
+
+        return text;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return true;
+    }
+}
