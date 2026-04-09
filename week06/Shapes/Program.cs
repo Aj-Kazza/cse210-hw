512651b22161b2866d1badd3dc5c5a792ddbe310
Aj-Kazza <akatia@byupathway.edu>
2026-04-09 23:57:53 +1200

    finished w06 shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square(2, "blue");
+        Rectangle rectangle = new Rectangle(2, 3, "red");
+        Circle circle = new Circle(3, "yellow");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
+
+        
     }
 }
---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    public Rectangle(double length, double width, string color) : base(color)
+    {
+        _length = length;
+        _width = width;
+        SetColor(color);
+    }
+
+    public override double GetArea()
+    {
+        double area = _width * _length;
+        return area;
+    }
+    
+}
